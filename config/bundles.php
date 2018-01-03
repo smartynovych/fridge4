366c692feddbee7749979b8f95767441390712ba
Serhiy Martynovych <smartynovych@gmail.com>
2018-01-03 11:22:54 +0200

    Add security and profiler

--- config/bundles.php
+++ config/bundles.php
@@ -9,4 +9,6 @@ return [
     Overblog\GraphQLBundle\OverblogGraphQLBundle::class => ['all' => true],
     Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
     Overblog\GraphiQLBundle\OverblogGraphiQLBundle::class => ['dev' => true],
+    Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
+    Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
 ];
